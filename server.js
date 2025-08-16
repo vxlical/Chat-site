1ba3b5d111467d751aa9091676030cea1db027a8
vxlical <joshsos52@email.com>
2025-08-16 01:18:46 +0100

    Fix: serve static files + wildcard route

--- server.js
+++ server.js
@@ -28,3 +28,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
