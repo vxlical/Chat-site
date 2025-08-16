2313d9b9b704b07adf7fe27def80395e236d4714
vxlical <joshsos52@email.com>
2025-08-16 01:09:59 +0100

    Initial commit of working chat app

--- server.js
+++ server.js
@@ -8,27 +8,8 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
-});
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-io.on('connection', (socket) => {
   socket.on('join', (username) => {
     socket.username = username;
-    console.log(`${username} joined`);
   });
 
   socket.on('chat message', (msg) => {
@@ -42,7 +23,8 @@ io.on('connection', (socket) => {
     console.log(`${socket.username || 'A user'} disconnected`);
   });
 });
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
